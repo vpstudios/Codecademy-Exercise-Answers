1018f7ad7e34ad5475d2b812ccf950107dd2b7f7
vpstudios <vpstudios@sunrise.ch>
2016-06-09 16:29:46 +0200

    :pencil:
    Update README.md and code

--- Language Skills/jQuery/Introduction to jQuery/script.js
+++ Language Skills/jQuery/Introduction to jQuery/script.js
@@ -14,3 +14,77 @@ $(document).ready(function() {
        $(this).toggle(1000);
    }); 
 });
+
+
+//  2. The Document Object Model
+
+$(document).ready(function() {
+    $('div').fadeOut(1000);
+});
+
+// 3. Changing Targets
+
+$(document).ready(function() {
+    $("#green").fadeOut(1000);
+});
+// 4. What is jQuery?
+
+$(document).ready(function() {
+    $("#notready").fadeOut(1000);
+});
+
+// 5. Linking Your HTML and JavaScript Files see index.html
+
+// 6. Getting Started
+
+$(document).ready(function(){
+});
+
+// 7. The Functional Approach
+
+$(document).ready(function(){
+});
+
+// 8. Electr(on)ic Slide
+
+$(document).ready(function(){
+    $('div').slideDown('slow');
+});
+
+// 9. Mid-Lesson Breather
+
+//No Code Necessary
+
+// 10. At the Ready see index.html!
+
+$(document).ready();
+
+// 11. Get Yourself In...
+
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $('div').fadeTo('fast',1.00);
+    });
+});
+
+// 12. ...and Get Yourself Out!
+
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $('div').fadeTo('fast',1.00);
+    });
+    $('div').mouseleave(function() {
+        $('div').fadeTo('fast',0.5);
+    });
+});
+
+// 13. Eureka!
+
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $('div').fadeTo('slow',1.00);
+    });
+    $('div').mouseleave(function() {
+        $('div').fadeTo('slow',0.5);
+    });
+});
